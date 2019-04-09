60afb3109fdf2881684262f7539f0820cf8915a0
macOS <macos@iMac.local>
2019-04-09 13:07:55 +0200

    End of lesson 4.

---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by macOS on 4/9/19.
+//  Copyright © 2019 macOS. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
